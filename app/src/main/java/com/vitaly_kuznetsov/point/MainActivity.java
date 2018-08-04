3e9e50b2179e06f2b07ddabcceb3e280ee34ea93
Vitaly <vitaly.kuznetsov97@gmail.com>
2018-08-04 22:27:54 +0300

    first commit

---
+++ app/src/main/java/com/vitaly_kuznetsov/point/MainActivity.java
@@ -0,0 +1,16 @@
+package com.vitaly_kuznetsov.point;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
